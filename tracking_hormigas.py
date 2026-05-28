67236d3ce105069b59d95b5339bb2b03477a433f
Luis Felipe Velasco Chilito <luisfelipevelascochilito@gmail.com>
2026-05-27 19:03:26 -0500

    Se creo la funcion individualizar_hormigas()

--- tracking_hormigas.py
+++ tracking_hormigas.py
@@ -5,7 +5,9 @@ Created on Tue May 26 18:24:18 2026
 @author: luisf
 """
 import cv2 as cv
+from sklearn.cluster import DBSCAN
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def detectar_posibles_hormigas(frame):
@@ -21,16 +23,38 @@ def detectar_posibles_hormigas(frame):
     cond_green  = r > (1.35 * g)
     cond_blue   = r > (1.35 * b)
     cond_thresh = r > 45
+    cond_oscuro_r = r < 140
+    cond_oscuro_g = g < 90
+    cond_oscuro_b = b < 90
     
     #Combinacion de condiciones 
-    mask_hormigas = cond_green & cond_blue & cond_thresh
-    filas=frame.shape[1]
+    mask_hormigas = cond_green & cond_blue & cond_thresh & cond_oscuro_r & cond_oscuro_g & cond_oscuro_b  
+    filas=frame.shape[0]
     y_indices, x_indices = np.where(mask_hormigas)
     for x, y in zip(x_indices, y_indices):
         coordenadas.append([x, filas - y])
         
-    return coordenadas
+    return np.array(coordenadas)
+
+def individualizar_hormigas(coordenadas):
     
+    #Individualizacion con DBSCAN
+    clustering = DBSCAN(eps=3, min_samples=20).fit(coordenadas)
+    
+    #Clusters  correspondiente a cada punto y ruido
+    labels=clustering.labels_
+    
+    #Depuracion de clusters que probablemente son ruido
+    promedio_puntos_por_cluster=(len(labels)-(np.count_nonzero(labels == -1)))/(len(np.unique(labels))-1)
+    unicos_labels, cantidad = np.unique(labels, return_counts=True)
+    es_pequeño= (cantidad < promedio_puntos_por_cluster ) & (unicos_labels != -1)
+    labels_a_eliminar = unicos_labels[es_pequeño]
+    labels[np.isin(labels, labels_a_eliminar)] = -1
+    
+    #Colores para diferenciar entre ruido (Gris) y clusters (Rojo)
+    colours = [(0.5, 0.5, 0.5) if label == -1 else (1, 0, 0) for label in labels]
+    return colours
+
 
 #Lectura de video de hormigas y despliegue de video procesado
 
@@ -41,6 +65,13 @@ def main():
         is_true,frame=video.read()
         if is_true:
             coordenadas_posibles_hormigas=detectar_posibles_hormigas(frame)
+            
+            #Presentacion de hormigas individualizadas 
+            coordenadas_x=coordenadas_posibles_hormigas[:,0]
+            coordenadas_y=coordenadas_posibles_hormigas[:,1]
+            colours=individualizar_hormigas(coordenadas_posibles_hormigas)
+            plt.scatter(coordenadas_x,coordenadas_y,c=colours,s=2)
+            plt.show()
             cv.imshow("Video",frame)
             if cv.waitKey(1) & 0xFF==ord('d'):
                 break

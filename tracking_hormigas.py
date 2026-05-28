1ae7c3af500952857df4262dca98fafc28a8f2e1
Luis Felipe Velasco Chilito <luisfelipevelascochilito@gmail.com>
2026-05-28 17:57:55 -0500

    Se creo la funcion punto_central_frame

--- tracking_hormigas.py
+++ tracking_hormigas.py
@@ -61,27 +61,68 @@ def coordenadas_en_los_extremos_de_cluster(labels,coordenadas_x,coordenadas_y,l)
     y_de_l=coordenadas_y[labels==l]
     return  min(x_de_l),max(x_de_l),min(y_de_l),max(y_de_l)
 
+def aplicar_mascara_frame(frame,coordenadas):
+    h, w = frame.shape[:2]
+    
+    #Definicion de la mascara negra
+    mask = np.zeros((h, w), dtype=np.uint8)
+    
+    #Cracion de rectangulo blanco con las cordenadas del area visible
+    cv.rectangle(mask, coordenadas[0], coordenadas[1], 255, thickness=-1)
+    
+    #Aplicacion de mascara en el frame
+    return cv.bitwise_and(frame, frame, mask=mask)
+
+def punto_central_frame(labels,coordenadas_x,coordenadas_y,l):
+    #Coordenadas x ,  y de un especifico label
+    x_de_l=coordenadas_x[labels==l]
+    y_de_l=coordenadas_y[labels==l]
+    
+    #Calcula el promedio de puntos ,que es equivalente al punto central
+    return  (int(sum(x_de_l)/len(x_de_l)),int(sum(y_de_l)/len(x_de_l)))
+    
+    
 #Lectura de video de hormigas y despliegue de video procesado
 
 def main():
+    #Lectura primer frame
     video=cv.VideoCapture("ant_video.mp4")
     is_true,frame=video.read()
+    
+    #Deteccion de hormigas
     coordenadas_posibles_hormigas=detectar_posibles_hormigas(frame)
-    coordenadas_x=coordenadas_posibles_hormigas[:,0]
-    coordenadas_y=coordenadas_posibles_hormigas[:,1]
     labels_hormigas=individualizar_hormigas(coordenadas_posibles_hormigas)
     
-    #Dibujo de areas de deteccion de hormigas
-    for l in np.unique(labels_hormigas):
-        if l!=-1:
-            x_minimo_de_l,x_maximo_de_l,y_minimo_de_l,y_maximo_de_l=coordenadas_en_los_extremos_de_cluster(labels_hormigas, coordenadas_x, coordenadas_y, l)
-            cv.rectangle(frame,(x_minimo_de_l-10,y_minimo_de_l-10),(x_maximo_de_l+10,y_maximo_de_l+10), (0, 255, 0), 2)
-            cv.putText(frame,f"{l}",(x_minimo_de_l-10,y_minimo_de_l-16), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-    cv.imshow("Primer frame", frame)
+    
+    #Dibujo de rectangulos de deteccion de hormigas
+    coordenadas_x=coordenadas_posibles_hormigas[:,0]
+    coordenadas_y=coordenadas_posibles_hormigas[:,1]
+    coordenadas_bordes_rectangulos=[]
+    lista_labels=np.unique(labels_hormigas).tolist()
+    lista_labels.remove(-1)
+    frame_con_rectangulos=frame.copy()
+    for l in lista_labels:
+        x_minimo_de_l,x_maximo_de_l,y_minimo_de_l,y_maximo_de_l=coordenadas_en_los_extremos_de_cluster(labels_hormigas, coordenadas_x, coordenadas_y, l)
+        esquina_superior_izquierda=(x_minimo_de_l-10,y_minimo_de_l-10)
+        esquina_inferior_derecha=(x_maximo_de_l+10,y_maximo_de_l+10)
+        cv.rectangle(frame_con_rectangulos,esquina_superior_izquierda,esquina_inferior_derecha, (0, 255, 0), 2)
+        cv.putText(frame_con_rectangulos,f"{l}",esquina_superior_izquierda, cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        coordenadas_bordes_rectangulos.append([esquina_superior_izquierda, esquina_inferior_derecha])
+    cv.imshow("Primer frame", frame_con_rectangulos)
+    cv.waitKey(0)
+    cv.destroyAllWindows()    
+    
+    #Seleccion de label a trackear 
+    label_a_trackear=int(input(f"Selecciona uno de los siguientes labels para ver su recorrido: {lista_labels}  "))
+    
+    #Aplicacion de mascara para enfocarse en el label a trackear
+    coordenadas_rectangulo_label=coordenadas_bordes_rectangulos[lista_labels.index(label_a_trackear)]
+    coordenadas_mascara=[(coordenadas_rectangulo_label[0][0]-100,coordenadas_rectangulo_label[0][1]-100),(coordenadas_rectangulo_label[1][0]+100,coordenadas_rectangulo_label[1][1]+100)]
+    frame=aplicar_mascara_frame(frame, coordenadas_mascara)
+    frame_con_punto=cv.circle(frame.copy(), punto_central_frame(labels_hormigas,coordenadas_x,coordenadas_y,label_a_trackear), 2, (0,0,255),5)
+    cv.imshow("Trackeo", frame_con_punto)
     cv.waitKey(0)
     cv.destroyAllWindows()
-    label_a_trackear=input(f"Selecciona uno de los siguientes labels para ver su recorrido: {np.unique(labels_hormigas)}")
-
     
     
     # coordenadas_x=coordenadas_posibles_hormigas[:,0]
